4eb22ad9accc8bb3db007e3fee52c422eef589b5
Stephan Göppentin <dedda102@gmail.com>
2021-02-09 17:44:15 +0100

    use correct path separator to load native library

--- src/main/kotlin/org/dedda/cellar/core/core.kt
+++ src/main/kotlin/org/dedda/cellar/core/core.kt
@@ -1,12 +1,13 @@
 package org.dedda.cellar.core
 
 import org.apache.commons.lang3.SystemUtils
+import java.io.File
 import java.lang.RuntimeException
 
 private var coreLoaded = false
 
 fun getLibraryPath(): String {
-    return System.getProperty("user.dir") + "/core/target/debug/" +
+    return System.getProperty("user.dir") + "/core/target/debug/".replace('/', File.separatorChar) +
     when (true) {
         SystemUtils.IS_OS_WINDOWS -> "cellar_core.dll"
         SystemUtils.IS_OS_LINUX -> "libcellar_core.so"
